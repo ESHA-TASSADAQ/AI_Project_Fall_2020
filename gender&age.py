57cecee6108151de8c3f91f0d40912010e8cc8a2
ESHA-TASSADAQ <eshatassadaq90@gmail.com>
2021-01-13 22:23:20 +0500

    update

--- gender&age.py
+++ gender&age.py
@@ -1,3 +1,15 @@
 import cv2
 import math
 import argparse
+
+parser=argparse.ArgumentParser()
+parser.add_argument('--image')
+
+args=parser.parse_args()
+
+faceProto="opencv_face_detector.pbtxt"
+faceModel="opencv_face_detector_uint8.pb"
+ageProto="age_deploy.prototxt"
+ageModel="age_net.caffemodel"
+genderProto="gender_deploy.prototxt"
+genderModel="gender_net.caffemodel"
