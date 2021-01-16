e88a6ce31fdaa4851aac21caa59579d68b2f624f
ESHA-TASSADAQ <eshatassadaq90@gmail.com>
2021-01-16 14:29:04 +0500

    model

--- gender&age.py
+++ gender&age.py
@@ -13,3 +13,23 @@ ageProto="age_deploy.prototxt"
 ageModel="age_net.caffemodel"
 genderProto="gender_deploy.prototxt"
 genderModel="gender_net.caffemodel"
+
+MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
+ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+genderList=['Male','Female']
+
+faceNet=cv2.dnn.readNet(faceModel,faceProto)
+ageNet=cv2.dnn.readNet(ageModel,ageProto)
+genderNet=cv2.dnn.readNet(genderModel,genderProto)
+
+video=cv2.VideoCapture(args.image if args.image else 0)
+padding=20
+while cv2.waitKey(1)<0:
+    hasFrame,frame=video.read()
+    if not hasFrame:
+        cv2.waitKey()
+        break
+
+    resultImg,faceBoxes=highlightFace(faceNet,frame)
+    if not faceBoxes:
+        print("No face detected")
