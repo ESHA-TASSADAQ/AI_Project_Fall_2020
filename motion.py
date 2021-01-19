9cc1920815eed274d68264a083ae8db819bff5b2
Tayyaba0 <shakirtayyaba07@gmail.com>
2021-01-20 01:40:11 +0500

    Motion time status and showing frames

--- motion.py
+++ motion.py
@@ -25,3 +25,25 @@ while True:
     thresh_frame=cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1] #Provides a threshold value, such that it will convert the difference value with less than 30 to black. If the difference is greater than 30 it will convert those pixels to white
     thresh_frame=cv2.dilate(thresh_frame, None, iterations=2) 
     (cnts,_)=cv2.findContours(thresh_frame.copy(),cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #Defines the contour area. Basically add the borders
+
+    for contour in cnts: #Removes noises and shadows. Basically, it will keep only that part white, which has area greater than 1000 pixels
+        if cv2.contourArea(contour) < 10000:
+            continue
+        status=1 #Change in status when the object is being detected
+
+        (x, y, w, h)=cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0,255,0), 3) #Creates a rectangular box around the object in the frame
+
+    status_list.append(status)  #List of status for every frame
+    status_list=status_list[-2:]
+
+    #Record datatime in a list when change occurs
+    if status_list[-1]==1 and status_list[-2]==0:
+        times.append(datetime.now())
+    if status_list[-1]==0 and status_list[-2]==1:
+        times.append(datetime.now())
+
+    cv2.imshow("Gray Frame",gray) 
+    cv2.imshow("Delta Frame",delta_frame)
+    cv2.imshow("Threshold Frame",thresh_frame)
+    cv2.imshow("Color Frame",frame) 
