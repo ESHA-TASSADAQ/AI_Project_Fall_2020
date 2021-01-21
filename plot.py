2d3f192fa12191898b7d52c1f71890b2438fea94
Tayyaba0 <shakirtayyaba07@gmail.com>
2021-01-22 00:11:18 +0500

    Setting the size,title, responsiveness of the graph and truning on hover effect

--- plot.py
+++ plot.py
@@ -8,3 +8,11 @@ df["End_string"]=df["End"].dt.strftime("%Y-%m-%d %H:%M:%S")
 
 
 cds=ColumnDataSource(df)
+
+#Set the size,title, responsiveness of the graph
+p=figure(x_axis_type='datetime',height=100, width=500, sizing_mode = "scale_width", title="Motion Graph")
+p.yaxis.minor_tick_line_color=None
+p.yaxis.ticker.desired_num_ticks = 1
+
+hover=HoverTool(tooltips=[("Start","@Start_string"),("End","@End_string")])
+p.add_tools(hover) #Turn on hover effect
